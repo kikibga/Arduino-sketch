2b50f005ac0df6e6d87de5458a96599efd6cc487
BlackWolf <45656455+kikibga@users.noreply.github.com>
2020-10-12 11:39:54 +0200

    ESP32Cam

---
+++ RobotCamESP32/RobotCamESP32.ino
@@ -0,0 +1,270 @@
+#include "src/OV2640.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+
+#include "src/SimStreamer.h"
+#include "src/OV2640Streamer.h"
+#include "src/CRtspSession.h"
+
+//#define SOFTAP_MODE // If you want to run our own softap turn this on
+#define ENABLE_WEBSERVER
+#define ENABLE_RTSPSERVER
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_ESP_EYE
+//#define CAMERA_MODEL_M5STACK_PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE
+#define CAMERA_MODEL_AI_THINKER
+
+#include "camera_pins.h"
+
+OV2640 cam;
+
+#ifdef ENABLE_WEBSERVER
+WebServer server(82);
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+WiFiServer rtspServer(8554);
+#endif
+
+
+#ifdef SOFTAP_MODE
+IPAddress apIP = IPAddress(192, 168, 1, 1);
+#else
+#include "wifikeys.h"
+#endif
+
+#ifdef ENABLE_WEBSERVER
+void handle_jpg_stream(void)
+{
+    WiFiClient client = server.client();
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+    server.sendContent(response);
+
+    while (1)
+    {
+        cam.run();
+        if (!client.connected())
+            break;
+        response = "--frame\r\n";
+        response += "Content-Type: image/jpeg\r\n\r\n";
+        server.sendContent(response);
+
+        client.write((char *)cam.getfb(), cam.getSize());
+        server.sendContent("\r\n");
+        if (!client.connected())
+            break;
+    }
+}
+
+void handle_jpg(void)
+{
+    WiFiClient client = server.client();
+
+    cam.run();
+    if (!client.connected())
+    {
+        return;
+    }
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-disposition: inline; filename=capture.jpg\r\n";
+    response += "Content-type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+    client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound()
+{
+    String message = "Server is running!\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    server.send(200, "text/plain", message);
+}
+#endif
+void startCameraServer();
+void setup() {
+
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println(); 
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  //initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);//flip it back
+    s->set_brightness(s, 1);//up the blightness just a bit
+    s->set_saturation(s, -2);//lower the saturation
+  }
+  //drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+  
+    IPAddress ip;
+
+#ifdef SOFTAP_MODE
+    const char *hostname = "devcam";
+   // WiFi.hostname(hostname); // FIXME - find out why undefined
+    WiFi.mode(WIFI_AP);
+    
+    bool result = WiFi.softAP(hostname, "12345678", 1, 0);
+    delay(2000);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    
+    if (!result)
+    {
+        Serial.println("AP Config failed.");
+        return;
+    }
+    else
+    {
+        Serial.println("AP Config Success.");
+        Serial.print("AP MAC: ");
+        Serial.println(WiFi.softAPmacAddress());
+
+        ip = WiFi.softAPIP();
+
+        Serial.print("Stream Link: rtsp://");
+        Serial.print(ip);
+        Serial.println(":8554/mjpeg/1");
+    }
+#else
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(F("."));
+    }
+    ip = WiFi.localIP();
+    Serial.println(F("WiFi connected"));
+    Serial.println("");
+    Serial.println(ip);
+    Serial.print("Stream Link: rtsp://");
+    Serial.print(ip);
+    Serial.println(":8554/mjpeg/1");  
+    startCameraServer();
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+
+#endif
+
+#ifdef ENABLE_WEBSERVER
+    server.on("/", HTTP_GET, handle_jpg_stream);
+    server.on("/jpg", HTTP_GET, handle_jpg);
+    server.onNotFound(handleNotFound);
+    server.begin();
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+    rtspServer.begin();
+#endif
+}
+
+CStreamer *streamer;
+CRtspSession *session;
+WiFiClient client; // FIXME, support multiple clients
+
+void loop()
+{
+#ifdef ENABLE_WEBSERVER
+    server.handleClient();
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+    uint32_t msecPerFrame = 100;
+    static uint32_t lastimage = millis();
+
+    // If we have an active client connection, just service that until gone
+    // (FIXME - support multiple simultaneous clients)
+    if(session) {
+        session->handleRequests(0); // we don't use a timeout here,
+        // instead we send only if we have new enough frames
+
+        uint32_t now = millis();
+        if(now > lastimage + msecPerFrame || now < lastimage) { // handle clock rollover
+            session->broadcastCurrentFrame(now);
+            lastimage = now;
+
+            // check if we are overrunning our max frame rate
+            now = millis();
+            if(now > lastimage + msecPerFrame)
+                printf("warning exceeding max frame rate of %d ms\n", now - lastimage);
+        }
+
+        if(session->m_stopped) {
+            delete session;
+            delete streamer;
+            session = NULL;
+            streamer = NULL;
+        }
+    }
+    else {
+        client = rtspServer.accept();
+
+        if(client) {
+            //streamer = new SimStreamer(&client, true);             // our streamer for UDP/TCP based RTP transport
+            streamer = new OV2640Streamer(&client, cam);             // our streamer for UDP/TCP based RTP transport
+
+            session = new CRtspSession(&client, streamer); // our threads RTSP session and state
+        }
+    }
+#endif
+}
